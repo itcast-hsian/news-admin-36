8c0123808178df59eca3894c9027eca6396142f0
itcast-hsian <541510140@qq.com>
2019-10-07 11:29:27 +0800

    项目的基本结构

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 可以忽略，生产环境时候是否有提示
 Vue.config.productionTip = false
 
+// $mount吧根实例挂载到指定的节点中，相当于el
 new Vue({
   router,
   render: h => h(App)
--- src/router.js
+++ src/router.js
@@ -1,25 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
+// 注册路由插件
 Vue.use(Router)
 
+// 创建路由对象
 export default new Router({
+  // 可忽略，默认的声明
   mode: 'history',
+  // 可忽略
   base: process.env.BASE_URL,
+  // 路由的配置
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    
   ]
 })

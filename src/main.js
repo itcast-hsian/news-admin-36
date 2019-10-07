e764311ab573cbfd613d48a5a7dd1bdf64a52604
itcast-hsian <541510140@qq.com>
2019-10-07 16:06:38 +0800

    添加路由守卫

--- src/main.js
+++ src/main.js
@@ -15,6 +15,27 @@ Vue.prototype.$axios = axios;
 // 设置基准路径
 axios.defaults.baseURL = "http://localhost:3000";
 
+// 路由守卫
+router.beforeEach((to, from, next) => {
+
+  // 如果不是登录页，都必须判断是否有token
+  if(to.path !== "/login"){
+
+    // 是否有token
+    const token = JSON.parse(localStorage.getItem("user") || `{}`).token;
+
+    if(token){
+      // 正常跳转
+      next()
+    }else{
+      next("/login")
+    }
+  }else{
+    // 访问的是登录页
+    next();
+  }
+})
+
 // 注册element插件
 Vue.use(ElementUI);
 
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Login from "@/views/Login"
+import Index from "@/views/Index"
 
 // 注册路由插件
 Vue.use(Router)
@@ -14,6 +15,10 @@ export default new Router({
   base: process.env.BASE_URL,
   // 路由的配置
   routes: [
-    { path: "/login", component: Login}
+    // 登录页
+    { path: "/login", component: Login},
+
+    // 后台首页
+    { path: "/", component: Index }
   ]
 })

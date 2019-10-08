12a2d7c34047a9cea819e87d21005306112bbc8c
itcast-hsian <541510140@qq.com>
2019-10-08 14:51:07 +0800

    发布文章的页面

--- src/views/PostAdd.vue
+++ src/views/PostAdd.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>456</div>
+  <div style="width:80%; margin-top:30px;">
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="标题">
+        <el-input v-model="form.title"></el-input>
+      </el-form-item>
+
+      <el-form-item label="内容">
+        <el-input
+        type="textarea"
+        :rows="2"
+        placeholder="请输入内容"
+        v-model="form.content">
+      </el-input>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      // 表单的数据对象
+      form: {
+        title: "", // 标题
+        content: "",
+        categories: [],
+        cover: [],
+        type: 1
+      }
+    }
+  }
 }
 </script>
 

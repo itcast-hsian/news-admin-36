d873cddd8bcf6d215297cde9a5042fc616d417cb
itcast-hsian <541510140@qq.com>
2019-10-08 09:51:13 +0800

    element表格的使用

--- src/views/PostList.vue
+++ src/views/PostList.vue
@@ -1,13 +1,63 @@
 <template>
-  <div>123</div>
+  <!-- data: 列表的数据对象 -->
+  <el-table
+    :data="tableData"
+    style="width: 100%">
+
+    <!-- 表格的列 -->
+    <!-- label：标签 -->
+    <el-table-column
+      label="日期"
+      width="180">
+
+      <!-- scope.row：数组中当前列的数据对象 -->
+      <template slot-scope="scope">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{ scope.row.date }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column
+      label="姓名"
+      width="180">
+      <template slot-scope="scope">
+        <span>{{scope.row.name}}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
-export default {
-  
-}
+  export default {
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }]
+      }
+    },
+    methods: {
+      // index是索引， row是对象
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      }
+    }
+  }
 </script>
-
-<style>
-
-</style>

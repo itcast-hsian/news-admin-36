8ff90b77e97ec28110322925de8e52d1c2c17a5b
itcast-hsian <541510140@qq.com>
2019-10-08 16:04:19 +0800

    图片上传组件的使用

--- src/views/PostAdd.vue
+++ src/views/PostAdd.vue
@@ -1,13 +1,48 @@
 <template>
   <div style="padding:30px;">
     <el-form :model="form" label-width="80px">
-      <el-form-item label="活动名称">
+      <el-form-item label="标题">
         <el-input v-model="form.title"></el-input>
       </el-form-item>
+    
+      <div>
+        <vueEditor :config="config"/>
+      </div>
+
+      <el-form-item label="栏目">
+        <!-- 栏目的数据来自于后台 -->
+        <el-checkbox-group v-model="form.categories">
+          <el-checkbox 
+          v-for="(item, index) in allCate"
+          :key="index"
+          v-if="item.id !== 999"
+          :label="item.id" 
+          name="type">{{item.name}}</el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+
+      <el-form-item label="封面">
+        <!-- action：上传图片的链接
+        list-type: 声明当前是上传多张图片
+        on-success： 图片上传成功的函数
+        on-remove: 移除图片函数 -->
+        <el-upload
+          action="http://localhost:3000/upload"
+          name="file"
+          :headers="{
+             Authorization: token
+          }"
+          list-type="picture-card"
+          :on-success="handleSuccess"
+          :on-remove="handleRemove">
+          <i class="el-icon-plus"></i>
+        </el-upload>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+      </el-form-item>
     </el-form>
-    <div>
-    <vueEditor :config="config"/>
-    </div>
   </div>
 </template>
 <script>
@@ -18,9 +53,18 @@ export default {
   name: 'app',
   data(){
     return {
+      // 提交的表单数据
       form: {
-
+        title: "",
+        content: "",
+        categories: [],
+        cover: []
       },
+
+      // 栏目的列表
+      allCate: [],
+      // token
+      token: JSON.parse(localStorage.getItem(`user`) || `{}`).token,
 
       config: {
         modules: { 
@@ -72,6 +116,32 @@ export default {
   components: {
     vueEditor
   },
+
+  methods: {
+    onSubmit(){
+      console.log(this.form.categories)
+    },
+
+    // 移除图片时候触发的函数
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    // 图片上传成功的回调函数
+    handleSuccess(res, file){
+      console.log(res, file)
+    }
+  },
+
+  mounted(){
+    // 请求栏目的数据
+    this.$axios({
+      url: "/category"
+    }).then(res => {
+      const {data} = res.data;
+
+      this.allCate = data;
+    })
+  }
 }
 </script>
 
